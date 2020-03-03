e7da3294b61596f942f1860a2c728372e32daedc
Danil Kravchenko <danil.kravchenko@chisw.com>
2020-03-03 16:21:20 +0200

    Returned controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,36 @@ class ProductsController < ApplicationController
 
     render json: @products
   end
+  # GET /products/1
+  def show
+    render json: @product
+  end
 
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created, location: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    if @product.update(product_params)
+      render json: @product
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    @product.destroy
+  end
+  
   private
     def set_product
       @product = Product.find(params[:id])
